af7b20d6f35ba529b8f7734b8a784289d3637470
Thinh <htthinh0@gmail.com>
2024-06-13 02:33:39 +0700

    Add a custom marker. Modify theme app to use only light theme.

--- app/src/main/java/com/htthinhus/gpstracker/InteractiveMapFragment.kt
+++ app/src/main/java/com/htthinhus/gpstracker/InteractiveMapFragment.kt
@@ -1,23 +1,32 @@
 package com.htthinhus.gpstracker
 
+import android.animation.ValueAnimator
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.scale
 import com.htthinhus.gpstracker.databinding.FragmentInteractiveMapBinding
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.OnMapClickListener
 
-class InteractiveMapFragment : Fragment() {
+class InteractiveMapFragment : Fragment(), OnMapClickListener {
 
     private var _binding: FragmentInteractiveMapBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var mapboxMap: MapboxMap
+    private var animator: ValueAnimator? = null
+    private val currentPoint = Point.fromLngLat(107.080641, 10.344681)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,20 +42,38 @@ class InteractiveMapFragment : Fragment() {
 
         mapboxMap.loadStyle(
             style(Style.OUTDOORS){
-
             }
         ) {
             val cameraOptions = CameraOptions.Builder()
-                .center(Point.fromLngLat(107.080641, 10.344681))
+                .center(currentPoint)
                 .zoom(15.0)
                 .build()
             mapboxMap.setCamera(cameraOptions)
         }
+
+        initializeMarker()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapClick(point: Point): Boolean {
+        return true
+    }
+
+    private fun initializeMarker() {
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.red_marker)
+        val annotationApi = binding.mapView.annotations
+        val pointAnnotationManager = annotationApi.createPointAnnotationManager()
+        val pointAnnotationOptions = PointAnnotationOptions()
+            .withPoint(currentPoint)
+            .withIconImage(
+                bitmap.scale(50, 70)
+            )
+        val myPointAnnotation = pointAnnotationManager.create(pointAnnotationOptions)
+
     }
 
 }
