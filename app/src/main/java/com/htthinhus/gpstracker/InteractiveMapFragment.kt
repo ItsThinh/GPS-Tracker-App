9e5a1c9a14f4ec683272cf632bff69d247a08168
Thinh <htthinh0@gmail.com>
2024-06-12 17:48:22 +0700

    implement map by Mapbox

--- app/src/main/java/com/htthinhus/gpstracker/InteractiveMapFragment.kt
+++ app/src/main/java/com/htthinhus/gpstracker/InteractiveMapFragment.kt
@@ -5,55 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.htthinhus.gpstracker.databinding.FragmentInteractiveMapBinding
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.style
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InteractiveMapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InteractiveMapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentInteractiveMapBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mapboxMap: MapboxMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_interactive_map, container, false)
+        _binding = FragmentInteractiveMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InteractiveMapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InteractiveMapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapboxMap = binding.mapView.mapboxMap
+
+        mapboxMap.loadStyle(
+            style(Style.OUTDOORS){
+
             }
+        ) {
+            val cameraOptions = CameraOptions.Builder()
+                .center(Point.fromLngLat(107.080641, 10.344681))
+                .zoom(15.0)
+                .build()
+            mapboxMap.setCamera(cameraOptions)
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
