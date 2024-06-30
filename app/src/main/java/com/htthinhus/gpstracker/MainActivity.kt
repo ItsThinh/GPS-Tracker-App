700948b2951595346afdae4d7a66f0bbd0fe89b3
Thinh <htthinh0@gmail.com>
2024-06-30 10:29:04 +0700

    Implement Cloud Function & FCM

--- app/src/main/java/com/htthinhus/gpstracker/MainActivity.kt
+++ app/src/main/java/com/htthinhus/gpstracker/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.htthinhus.gpstracker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,5 +41,50 @@ class MainActivity : AppCompatActivity() {
                 }
         }
 
+        val USER_UID = "naw7Ba6apLNIstW0sm491NGXx3G2"
+        val firestoreRef = FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(USER_UID)
+            .collection("fcmTokens")
+            .document("token1234")
+
+        firestoreRef.set(emptyMap<String, Any>())
+            .addOnSuccessListener {
+                Log.d("SENDING_FCM_TOKEN", "Token sent")
+            }
+            .addOnFailureListener {
+                Log.d("SENDING_FCM_TOKEN", it.toString())
+            }
+
+        createNotificationChannel()
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM_TOKEN", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("FCM_TOKEN", token)
+            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    private fun createNotificationChannel() {
+        val name = "Warning Channel"
+        val descriptionText = "Notify when vehicle status change"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        val CHANNEL_ID = "CHANNEL_1"
     }
 }
---
+++ app/src/main/java/com/htthinhus/gpstracker/MyFirebaseMessagingService.kt
@@ -0,0 +1,61 @@
+package com.htthinhus.gpstracker
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        sendTokenToFirestore(token)
+        Log.d("SENDING_FCM_TOKEN", token)
+    }
+
+    private fun sendTokenToFirestore(token: String) {
+
+        val USER_UID = "naw7Ba6apLNIstW0sm491NGXx3G2" // change to UID of authentication later
+        val firestoreRef = FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(USER_UID)
+            .collection("fcmTokens")
+            .document(token)
+
+        firestoreRef.set(emptyMap<String, Any>())
+            .addOnSuccessListener {
+                Log.d("SENDING_FCM_TOKEN", "Token sent")
+            }
+            .addOnFailureListener {
+                Log.d("SENDING_FCM_TOKEN", it.toString())
+            }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val status:Boolean = message.data["status"].toBoolean()
+
+        val CHANNEL_ID = "CHANNEL_1"
+        val contentText = if (status) "VEHICLE TURN ON" else "VEHICLE TURN OFF"
+
+        var notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_vehicle_status)
+            .setContentTitle("VEHICLE STATUS")
+            .setContentText(contentText)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(1, notificationBuilder.build())
+    }
+}
